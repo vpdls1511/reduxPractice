513a2de4c43d485b02095521629fb6a12ac77d59
vpdls1511 <vpdls1511@gmail.com>
2021-04-04 23:28:32 +0900

    first commit

---
+++ src/component/InputBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const inputBox = () => {
+    return (
+        <React.Fragment>
+            <input type='text'> </input>
+        </React.Fragment>
+    )
+}
+
+
+export default inputBox();
